826e2cddc3d75f22ea4061fd185fee4a1338617f
hktalent <18223385+hktalent@users.noreply.github.com>
2024-10-20 08:43:08 +0800

    up 2024-10-20

---
+++ comfyui-colab/dev/terminal.py
@@ -0,0 +1,21 @@
+from subprocess import getoutput
+
+class Terminal:
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {"image": ("IMAGE",), "text": ("STRING", {"multiline": True})}}
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "execute"
+    def execute(self, image, text):
+        out = getoutput(f"{text}")
+        print(out)
+        return (image, )
+
+    CATEGORY = "Tools"
+
+NODE_CLASS_MAPPINGS = {
+    "Terminal": Terminal
+}
